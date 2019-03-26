3ecf8187740d20490e6a2d24e0b90fe7aeaddbf1
Nemuel Leal Pereira <Nemolp_15@hotmail.com>
2019-03-25 21:33:41 -0400

    CoursControlleur Get By Id

--- CoursInformatique/CoursInformatique/Controllers/CoursController.cs
+++ CoursInformatique/CoursInformatique/Controllers/CoursController.cs
@@ -1,9 +1,25 @@
-﻿using System.Web.Http;
+﻿using CoursInformatique.Models;
+using System.Web.Http;
 
 namespace CoursInformatique.Controllers
 {
     public class CoursController : ApiController
     {
+        private CoursInformatiqueContext db = new CoursInformatiqueContext();
+
+        public IHttpActionResult GetCours(int Id)
+        {
+            if (Id < 0)
+                return BadRequest("Le Id doit être supérieur à zero"); /*400*/
+
+            var curso = db.Cours.Find(Id);
+
+            if (curso == null)
+                return NotFound(); /*404*/
+
+            return Ok(curso); /*200*/
+        }
+
         //TODO Get, GetId, Put Post
     }
 }
