969ff1e79b27b0e52f5f2b7e487b352ca8bb6248
Nemuel Leal Pereira <Nemolp_15@hotmail.com>
2019-03-25 20:54:44 -0400

    DbContext et Web.config terminé

---
+++ CoursInformatique/CoursInformatique/Models/CoursInformatiqueContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace CoursInformatique.Models
+{
+    public class CoursInformatiqueContext : DbContext
+    {
+        public CoursInformatiqueContext() : base("CoursInformatique")
+        {
+        }
+
+        public DbSet<Cours> Cours { get; set; }
+    }
+}
